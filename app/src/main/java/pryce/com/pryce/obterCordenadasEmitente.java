1a179a80f20140bbd626ad04d0bf96a8b59a2972
Gustavo O. Cardozo <gustavo_o_@hotmail.com>
2019-02-18 18:00:09 -0400

    OK.
    Correção em geolocation. 1
    Correção em geolocation. 2

--- app/src/main/java/pryce/com/pryce/obterCordenadasEmitente.java
+++ app/src/main/java/pryce/com/pryce/obterCordenadasEmitente.java
@@ -6,21 +6,20 @@ import java.net.URL;
 import java.util.Scanner;
 
 
-
 public class obterCordenadasEmitente {
 
 
     public static int nLinha = 0;
 
 
-    public void obterLatLog(String logradouro, String bairro, String numero, String cidade, String uf){
+    public void obterLatLog(String logradouro, String bairro, String numero, String cidade, String uf) {
         StringBuilder numeros = new StringBuilder();
 
         //http://maps.googleapis.com/maps/api/geocode/json?address=AV.+CAPITAO+SILVIO,3790,GRANDES+AREAS,ARIQUEMES,RO
         //https://maps.googleapis.com/maps/api/geocode/json?address=Rua+Gavi%C3%A3o+Real+4695+Ariquemes+RO&key=AIzaSyBTs9ShHOmdlmcFOqUAuxzes0nspUtUOW4
 //"https://maps.googleapis.com/maps/api/geocode/json?address="+logradouro.replaceAll(" ", "+")+"+"+numero+"+"+bairro.replaceAll(" ", "+")+"+"+cidade.replaceAll(" ", "+")+"+"+uf+"&key=AIzaSyBTs9ShHOmdlmcFOqUAuxzes0nspUtUOW4"
         try {
-            URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address="+logradouro.replaceAll(" ", "+")+"+"+numero+"+"+bairro.replaceAll(" ", "+")+"+"+cidade.replaceAll(" ", "+")+"+"+uf+"&key=AIzaSyBTs9ShHOmdlmcFOqUAuxzes0nspUtUOW4");
+            URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" + logradouro.replaceAll(" ", "+") + "+" + numero + "+" + bairro.replaceAll(" ", "+") + "+" + cidade.replaceAll(" ", "+") + "+" + uf + "&key=AIzaSyBTs9ShHOmdlmcFOqUAuxzes0nspUtUOW4");
             BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
             String minhaLinha;
             Emitente.lat = null;
@@ -31,32 +30,31 @@ public class obterCordenadasEmitente {
             }
 
 
-
-
             Scanner scan = new Scanner(numeros.toString());
 
             while (scan.hasNextLine()) {
                 String linha = scan.nextLine();
                 // Pegar lat e log
                 nLinha++;
-                if (nLinha == 54) {
+
+                if (linha.contains("lat")) {
                     linha = linha.substring(linha.indexOf("-"), linha.indexOf(","));
                     Emitente.lat = linha;
-
                 }
-                if (nLinha == 55) {
+                if (linha.contains("lng")) {
                     linha = linha.substring(linha.indexOf("-"), linha.length());
                     Emitente.log = linha;
                 }
-                if(Emitente.log != null && Emitente.lat != null){
+
+
+                if (Emitente.log != null && Emitente.lat != null) {
                     break;
                 }
 
             }
             scan.close();
 
-        }
-        catch (Exception localException){
+        } catch (Exception localException) {
             System.out.println(localException.toString());
 
 
